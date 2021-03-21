40087c9377484bd01860cf1f4f268226326d9bc7
larysson <larysson.alves@gmail.com>
2021-03-21 17:16:09 -0300

    list of exercises

---
+++ src/services/student-exercise-service.ts
@@ -0,0 +1,19 @@
+import { api } from '../shared/services/api'
+
+import { StudentExercise } from '../entities/student'
+
+const studentUrl = 'student/'
+
+const getStudentExercises = async (
+  studentId: number
+): Promise<StudentExercise[]> => {
+  const response = await api.get<StudentExercise[]>(
+    `${studentUrl}${studentId}/exercise/`
+  )
+
+  return response.data.map(
+    studentExercise => new StudentExercise(studentExercise)
+  )
+}
+
+export { getStudentExercises }
