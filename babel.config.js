bfbf5097991c63a459f0abfa009da515b7d749ef
larysson <larysson.alves@gmail.com>
2021-10-13 20:03:46 -0300

    .env to load variables, fix app icon

--- babel.config.js
+++ babel.config.js
@@ -1,11 +1,20 @@
-module.exports = function(api) {
-  api.cache(true);
+module.exports = function (api) {
+  api.cache(true)
+
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module:react-native-dotenv',
+        {
+          moduleName: 'react-native-dotenv',
+        },
+      ],
+    ],
     env: {
       production: {
         plugins: ['react-native-paper/babel'],
       },
     },
-  };
-};
+  }
+}
--- src/shared/services/api.ts
+++ src/shared/services/api.ts
@@ -1,27 +1,18 @@
 import axios from 'axios'
-
-import { navigate } from './navigation-service'
-
-import * as SecureStorage from '../../shared/services/secure-storage'
+import { API_URL } from 'react-native-dotenv'
 
 const api = axios.create({
-  baseURL: 'http://192.168.18.206:8000/',
-  // baseURL: 'https://bhavior-api.herokuapp.com/'
+  baseURL: API_URL,
 })
 
 api.interceptors.request.use(axiosRequestConfig => {
   const backSlashUrl = axiosRequestConfig.url?.endsWith('/')
 
   axiosRequestConfig.url = backSlashUrl ? axiosRequestConfig.url : `${axiosRequestConfig.url}/`
-
   return axiosRequestConfig
 })
 
 api.interceptors.response.use(undefined, async error => {
-  // if (error.response.status === 403) {
-  //   navigate('Login')
-  //   await SecureStorage.clearToLogout()
-  // }
   return Promise.reject(error)
 })
 
